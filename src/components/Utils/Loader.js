36d7c579fedaadf7482c4ad3d7909ff111145433
Lucas Delobelle <lucas@ld-web.net>
2019-11-25 11:48:18 +0100

    Loader formatting

--- src/components/Utils/Loader.js
+++ src/components/Utils/Loader.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import { DoubleBounce } from 'better-react-spinkit';
+import React from "react";
+import { DoubleBounce } from "better-react-spinkit";
 
 const Loader = () => (
   <div className="mt-3 d-flex justify-content-center">
-      <DoubleBounce size={60} color="#CCC" />
-    </div>
+    <DoubleBounce size={60} color="#CCC" />
+  </div>
 );
 
 export default Loader;
