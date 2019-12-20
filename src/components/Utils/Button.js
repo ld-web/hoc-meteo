ae875c12d03e0ada45d79b95a34867654ff5de52
Lucas Delobelle <lucas@ld-web.net>
2019-12-20 12:20:42 +0100

    Optimized Button write style

--- src/components/Utils/Button.js
+++ src/components/Utils/Button.js
@@ -2,9 +2,11 @@ import React, { Component } from "react";
 
 class Button extends Component {
   render() {
+    const { handleClick, type, textContent } = this.props;
+
     return (
-      <button onClick={this.props.handleClick} className={`btn btn-${this.props.type}`}>
-        {this.props.textContent}
+      <button onClick={handleClick} className={`btn btn-${type}`}>
+        {textContent}
       </button>
     );
   }
